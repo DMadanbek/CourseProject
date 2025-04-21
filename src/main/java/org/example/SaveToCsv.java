e47ec23eaf170421ef15926a2b8f632795c5bc6a
Madanbek <zonguckaevo@gmail.com>
2025-04-22 00:30:58 +0600

    1

---
+++ src/main/java/org/example/SaveToCsv.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.Vector;
+
+public class SaveToCsv{
+
+        int id;
+        String name;
+        String surname;
+        int age;
+        boolean acc;
+
+        public SaveToCsv(boolean acc,int age,String name,String surname,int id){
+            this.acc = acc;
+            this.age = age;
+            this.surname = surname;
+            this.id = id;
+            this.name = name;
+        }
+
+        public void Saver(String filename){
+
+        }
+
+}
+
+
+
